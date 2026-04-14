37e20780604f4c77a4f9e18c5ee7a9800c621850
Markury <git@markury.dev>
2026-04-14 01:37:23 -0400

    Initial Commit

---
+++ Slurp/SlurpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlurpApp.swift
+//  Slurp
+//
+//  Created by Mark on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct SlurpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
